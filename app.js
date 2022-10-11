568216c865ba6bed6f02de46f7b1349e2067a89e
Naureen <naureen.imran123@yahoo.com>
2022-10-11 07:11:24 +0100

    get api

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.get("/", getEndPoints);
+app.get("/api", getEndPoints);
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/users', getUser);
